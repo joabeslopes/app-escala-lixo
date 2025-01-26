f67e25488f6725935dac8953fd940bb04f4eefbe
joabeslopes <joabeslopes.jsl@gmail.com>
2025-01-26 17:08:09 -0300

    sctrict mode para melhorar desenvolvimento

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './main.css';
 import MainForm from './components/MainForm/MainForm.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <>
+  <StrictMode>
     <MainForm />
-  </>
+  </StrictMode>
 );
